0d22837ceb62b96b1dd666bc112cd14e19ad43f0
diegocos <diegobassay@gmail.com>
2022-02-15 20:10:33 -0300

    adding support to socketio-server

--- ip-address-management-api/src/main/java/com/diegobassay/ip/address/management/Application.java
+++ ip-address-management-api/src/main/java/com/diegobassay/ip/address/management/Application.java
@@ -1,5 +1,9 @@
 package com.diegobassay.ip.address.management;
 
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,8 +18,23 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan(basePackages={"com.diegobassay.ip.address.management.*"})
 public class Application {
 
+    @Value("${websocket-server.host}")
+    private String host;
+
+    @Value("${websocket-server.port}")
+    private Integer port;
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        Configuration config = new Configuration();
+        config.setHostname(host);
+        config.setPort(port);
+        return new SocketIOServer(config);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 }
+
